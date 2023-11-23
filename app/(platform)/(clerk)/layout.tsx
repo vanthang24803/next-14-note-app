5979e0b3779f5bd6b62ca9dd8c1e2fdf58661f13
vanthang24803 <nguyenthang24803@gmail.com>
2023-11-23 14:12:01 +0700

    update webhook api strapi and bg-layout auth

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -1,12 +1,9 @@
-const ClerkLayout = ({ children }: {
-    children: React.ReactNode;
-  }) => {
-    return (
-      <div className="h-full flex items-center justify-center">
-        {children}
-      </div>
-    );
-  };
-  
-  export default ClerkLayout;
-  
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex items-center justify-center bg-[url('https://images.unsplash.com/photo-1493934558415-9d19f0b2b4d2?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-center bg-cover ">
+      {children}
+    </div>
+  );
+};
+
+export default ClerkLayout;
